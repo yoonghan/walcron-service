fee3498cf3258ef565f1b7bff53039be7d63543e
Walcron <b_wars@yahoo.com>
2020-10-01 13:33:27 +0800

    Initial commit

---
+++ dist/kafka.js
@@ -0,0 +1,73 @@
+"use strict";
+var __awaiter = (this && this.__awaiter) || function (thisArg, _arguments, P, generator) {
+    function adopt(value) { return value instanceof P ? value : new P(function (resolve) { resolve(value); }); }
+    return new (P || (P = Promise))(function (resolve, reject) {
+        function fulfilled(value) { try { step(generator.next(value)); } catch (e) { reject(e); } }
+        function rejected(value) { try { step(generator["throw"](value)); } catch (e) { reject(e); } }
+        function step(result) { result.done ? resolve(result.value) : adopt(result.value).then(fulfilled, rejected); }
+        step((generator = generator.apply(thisArg, _arguments || [])).next());
+    });
+};
+var __importDefault = (this && this.__importDefault) || function (mod) {
+    return (mod && mod.__esModule) ? mod : { "default": mod };
+};
+Object.defineProperty(exports, "__esModule", { value: true });
+exports.withKafkaConsumer = exports.withKafkaProducer = exports.createKafkaConf = void 0;
+`use strict`;
+const kafkajs_1 = __importDefault(require("kafkajs"));
+function _getTopic(prefix) { return `${prefix}default`; }
+function createKafkaConf(brokerList, username, password) {
+    const config = {
+        clientId: 'my-app',
+        brokers: brokerList,
+        ssl: true,
+        connectionTimeout: 10000,
+        sasl: {
+            mechanism: 'scram-sha-256',
+            username: username,
+            password: password
+        }
+    };
+    return new kafkajs_1.default.Kafka(config);
+}
+exports.createKafkaConf = createKafkaConf;
+function withKafkaProducer(kafkaClient, prefix) {
+    return __awaiter(this, void 0, void 0, function* () {
+        const producer = kafkaClient.producer();
+        yield producer.connect();
+        const writer = (msg) => {
+            producer.send({
+                topic: _getTopic(prefix),
+                messages: [{ value: msg }]
+            });
+        };
+        return writer;
+    });
+}
+exports.withKafkaProducer = withKafkaProducer;
+function withKafkaConsumer(kafkaClient, prefix, groupId, writer) {
+    return __awaiter(this, void 0, void 0, function* () {
+        //Kafka guarantees that a message is only read by a single consumer in the group.
+        const consumer = kafkaClient.consumer({ groupId });
+        yield consumer.connect();
+        yield consumer.subscribe({ topic: _getTopic(prefix) });
+        const disconnect = () => {
+            consumer.close(true, () => { });
+        };
+        const _consumeMessage = () => {
+            consumer.run({
+                eachMessage: ({ topic, partition, message }) => __awaiter(this, void 0, void 0, function* () {
+                    // console.log({
+                    //     key: message.key.toString(),
+                    //     value: message.value.toString(),
+                    //     headers: message.headers,
+                    // })
+                    console.log(message.value.toString(), 'message');
+                }),
+            });
+        };
+        _consumeMessage();
+        return "";
+    });
+}
+exports.withKafkaConsumer = withKafkaConsumer;
---
+++ src/kafka.ts
@@ -0,0 +1,67 @@
+`use strict`
+import Kafka from 'kafkajs';
+
+function _getTopic(prefix:string) { return `${prefix}default`}
+
+export function createKafkaConf (
+  brokerList: Array<string>,
+  username: string,
+  password: string
+) {
+  const config = {
+    clientId: 'my-app',
+    brokers: brokerList,
+    ssl: true,
+    connectionTimeout: 10000,
+    sasl: {
+      mechanism: ('scram-sha-256' as any),
+      username: username,
+      password: password
+    }
+  };
+  return new Kafka.Kafka(config);
+}
+
+export async function withKafkaProducer(kafkaClient:any, prefix:string) {
+  const producer = kafkaClient.producer();
+  await producer.connect();
+
+  const writer = (msg:string) => {
+    producer.send(
+      {
+        topic: _getTopic(prefix),
+        messages: [{value: msg}]
+      }
+    );
+  }
+
+  return writer;
+}
+
+export async function withKafkaConsumer(kafkaClient:any, prefix:string, groupId:string, writer:(msg:string) => void) {
+  //Kafka guarantees that a message is only read by a single consumer in the group.
+  const consumer = kafkaClient.consumer({groupId});
+  await consumer.connect();
+  await consumer.subscribe({topic: _getTopic(prefix)});
+
+  const disconnect = () => {
+    consumer.close(true, () => {});
+  }
+
+  const _consumeMessage = () => {
+    consumer.run({
+      eachMessage: async ({ topic, partition, message }) => {
+        // console.log({
+        //     key: message.key.toString(),
+        //     value: message.value.toString(),
+        //     headers: message.headers,
+        // })
+        console.log(message.value.toString(), 'message');
+      },
+    })
+  }
+
+  _consumeMessage();
+
+  return "";
+}
