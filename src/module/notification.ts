3046500bbb7382d0134017e2882d0a593d738589
Walcron <b_wars@yahoo.com>
2020-10-05 12:14:59 +0800

    Update notification body

--- src/module/notification.ts
+++ src/module/notification.ts
@@ -25,11 +25,11 @@ export const userNotifier = async(airtable:any, orderId:string, status:EnumOrder
         "orderid": orderId,
         "status": status
       }
-      sendCloudMessageByRegistrationToken(pusherMessage, contactInfo, `Order ${orderId} is ${status}`, `Your order is ready for pick up, please go to the respective counter to pick it up.`);
+      sendCloudMessageByRegistrationToken(pusherMessage, contactInfo, `Order ${orderId} is ${status}`, `Your ${orderId} is ${status} for pick up, please go to the respective counter to pick it up.`);
       break;
     case 'Email':
       const subject = `Order ${orderId} is ${status}`;
-      const emailMessage = `Your order is ready for pick up, please go to the respective counter to pick it up.`;
+      const emailMessage = `Your <em>${orderId}</em> is <em>${status}</em> for pick up, please go to the respective counter to pick it up.`;
       const recipientTo = contactInfo;
       sendEmail(process.env.GMAIL_USER_EMAIL , process.env.GMAIL_PASSWORD, recipientTo, subject, emailMessage);
       break;
